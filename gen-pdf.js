f7c57debdf0d432ae5566b22762f9b36a915c8ce
白一梓 <yunnysunny@gmail.com>
2025-06-21 04:52:32 +0000

    add md-to-pdf dep[skip ci]

--- gen-pdf.js
+++ gen-pdf.js
@@ -1,8 +1,10 @@
 const markdownpdf = require('markdown-pdf')
 const path = require('path')
 const fs = require('fs')
+const { mdToPdf } = require('md-to-pdf')
 
-const bookPath = path.join(__dirname, 'output/book.pdf')
+
+// const bookPath = path.join(__dirname, 'output/book.pdf')
 const markdownPath = path.join(__dirname, 'zh-cn')
 
 // 递归遍历文件夹，返回所有 .md 文件的绝对路径数组
@@ -21,12 +23,31 @@ function getAllMarkdownFiles(dir) {
   return results
 }
 
+async function mergeMarkdownToPdf(mdFiles, outputPath) {
+  const mergedContent = mdFiles
+    .map(file => fs.readFileSync(file, 'utf-8'))
+    .join('\n\n---\n\n') // 分隔线（也可换成分页符）
+
+  await mdToPdf(
+    { content: mergedContent },
+    {
+      dest: outputPath,
+      pdf_options: {
+        format: 'A4',
+      },
+    }
+  )
+
+  console.log('✅ PDF 已生成:', outputPath)
+}
+
 const mdDocs = getAllMarkdownFiles(markdownPath)
 console.log(mdDocs)
 
-const paperFormat = 'A6'
-markdownpdf({
-  paperFormat,
-}).concat.from(mdDocs).to(bookPath, function () {
-  console.log('Created', paperFormat, bookPath)
-})
+// const paperFormat = 'A6'
+// markdownpdf({
+//   paperFormat,
+// }).concat.from(mdDocs).to(bookPath, function () {
+//   console.log('Created', paperFormat, bookPath)
+// })
+mergeMarkdownToPdf(mdDocs, path.join(__dirname, 'output/book.pdf'))
--- gen_pdf2.sh
+++ gen_pdf2.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+sudo apt update
+
+sudo apt install -y \
+  libatk-bridge2.0-0t64 \
+  libatk1.0-0t64 \
+  libcups2t64 \
+  libdrm2 \
+  libxcomposite1 \
+  libxdamage1 \
+  libxrandr2 \
+  libgbm1 \
+  libasound2t64 \
+  libpangocairo-1.0-0 \
+  libpango-1.0-0 \
+  libxss1 \
+  libgtk-3-0t64 \
+  libnss3 \
+  libxshmfence1 \
+  libx11-xcb1 \
+  libxext6 \
+  libxcb1 \
+  ca-certificates \
+  fonts-noto-cjk \
+  fonts-wqy-zenhei \
+  fonts-wqy-microhei
 
 export OPENSSL_CONF=/dev/null
 node gen-pdf.js
